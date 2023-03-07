908d4fca8428ae343c7719637b9c22f4abb2a6bb
nathan.blaga@dxc.com <nathan.blaga@dxc.com>
2023-03-07 15:10:07 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+ function changeImage() {
+        let elem = document.getElementById('image')
+    if (elem.style.visibility === 'hidden') {
+        elem.style.visibility = 'visible'
+    } else {
+        elem.style.visibility = 'hidden'
+    }
+    }
